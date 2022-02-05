43b820688c0d225202227b7b52fb6970b1052fac
Charisma Kausar <charisma.kausar@gmail.com>
2022-02-05 12:08:27 +0800

    Create initial GUI

--- src/main/java/heylo/Main.java
+++ src/main/java/heylo/Main.java
@@ -5,11 +5,15 @@ import java.util.Scanner;
 import heylo.commands.Command;
 import heylo.tasks.Task;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  * Drives the Heylo program.
  */
-public class Main {
+public class Main extends Application {
     /**
      * Loads previously saved tasks, greets the user and accepts commands to run.
      *
@@ -42,5 +46,14 @@ public class Main {
 
         System.out.println(logo);
         System.out.println("Heylo! What can I do for you today? :)\n");
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 }
